770fdf932c8dc5db1e6139a9701efcbdf3ecba09
Horst Schneider <h.schneider@hs-mannheim.de>
2016-07-08 16:24:02 +0200

    adds threshold

--- sigs.c
+++ sigs.c
@@ -2,31 +2,49 @@
 #include "unistd.h"
 #include "dir.c"
 
+static int threshold;
+
 void print_sig_survey(const char* filename) {
 	int c, count = 0;
 	FILE *file;
+
+	size_t size = 512;
+	size_t len = 0;
 	
+	char* buf;
+
+	buf = realloc(NULL, sizeof(char) * size); 
+
 	file = fopen(filename, "r");
 
 	if(file != NULL) {
-		printf("%s ", filename);
 
 		while((c = fgetc(file)) != EOF) {
 			if(c == '\n') {
 				count++;
 			}
 			if(c == '{' || c == '}' || c == ';') {
-				printf("%c", (char) c);
+				buf[len++] = c;
+				if(len == size) {
+					buf = realloc(buf, sizeof(char) * (size+=128)); 
+				}
 			}		
 		}
-		printf(" (%d)\n", count);
-
+		
 		fclose(file);
-	}
 
+		if(count >= threshold) {
+
+			buf[len++] = '\0';	
+
+			printf("%s ", filename);
+			printf("%s", buf);
+			printf(" (%d)\n", count);
+		}
+	}
 }
 
-void print_text(const char* filename) {
+void do_survey(const char* filename) {
 	char *dot;
 	dot  = strrchr(filename, '.');
 	if (dot && !strcmp(dot, ".java")) {
@@ -34,7 +52,14 @@ void print_text(const char* filename) {
 	}		
 }
 
+
 int main(int argc, char* argv[]) {
-	listdir(".", 0, print_text);
+	threshold = 0;
+
+	if(argc > 1) {
+		sscanf (argv[1],"%d", &threshold);
+	}
+
+	listdir(".", 0, do_survey);
 	return 0;
 }
