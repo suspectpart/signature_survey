5fa145421687ddf1f5b2106ee27b836f5aee6e38
Horst Schneider <dieterdervermieter@gmail.com>
2016-07-09 14:10:24 +0200

    adds some prototyping for html output

--- src/dir.c
+++ src/dir.c
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef void (*print_survey)(const char* filename);
+typedef void (*survey)(const char* filename);
 
-void listdir(const char *name, int level, print_survey prints)
+void listdir(const char *name, int level, survey survey)
 {
 	DIR *dir;
 	struct dirent *entry;
@@ -20,28 +20,23 @@ void listdir(const char *name, int level, print_survey prints)
 		return;
 	}
 
-    	do {
+	do {
 		if (entry->d_type == DT_DIR) {
 			char path[1024];
 			int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
 			path[len] = 0;
-            		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
 				continue;
 			}
 
-			listdir(path, level + 1, prints);
-        	}
+			listdir(path, level + 1, survey);
+		}
 		else {
-			//printf("%*s- %s\n", level*2, "", entry->d_name);
-			if(strcmp(name, ".") == 0) {
-				prints(entry->d_name);
-			} else {
-				char* fullPath = malloc(strlen(name) + strlen(entry->d_name) + 2);
-				strcpy(fullPath, name);
-				strcat(fullPath, "/");
-				strcat(fullPath, entry->d_name);
-				prints(fullPath);
-			}
+			char* fullPath = malloc(strlen(name) + strlen(entry->d_name) + 2);
+			strcpy(fullPath, name);
+			strcat(fullPath, "/");
+			strcat(fullPath, entry->d_name);
+			survey(fullPath);
 		}
     } while (entry = readdir(dir));
 
--- src/survey.c
+++ src/survey.c
@@ -6,12 +6,36 @@
 #include "options.h"
 
 int total_statements = 0;
+int first = 1;
+
+void print_to_stdout(const char* filename, int lines, const char* survey) {
+	printf("%s ", filename);
+	printf("%d ", lines);
+	printf("%s\n", survey);
+}
+
+void print_to_html(const char* filename, int lines, const char* survey) {
+	FILE* html;
+
+	if(first) {
+		html = fopen("sigs.html", "w+");
+		fprintf(html, "<!DOCTYPE html>");
+		fprintf(html, "<head><title>Signature Survey</title></head>");
+		fprintf(html, "<h1>Signature Survey for Project X</h1><br />");
+		fprintf(html, "<html>");
+		first = 0;
+	} else {
+		html = fopen("sigs.html", "a+");
+	}
+
+	fprintf(html, "<a href='%s'>%s (%d)</a>: %s<br />", filename, filename, lines, survey);
+	fclose(html);
+}
 
 void print_sig_survey(const char* filename) {
 	int c, lines = 0;
 	FILE *file;
-
-	size_t size = 512;
+	size_t size = 1024;
 	size_t len = 0;
 	
 	char* buf = realloc(NULL, sizeof(char) * size); 
@@ -31,20 +55,18 @@ void print_sig_survey(const char* filename) {
 			if(c == '{' || c == '}' || c == ';') {
 				buf[len++] = c;
 				if(len == size) {
-					buf = realloc(buf, sizeof(char) * (size+=128)); 
+					buf = realloc(buf, sizeof(char) * (size+=1024)); 
 				}
 			}		
 		}
 		
 		fclose(file);
 		
+		buf[len++] = '\0';	
+
 		if(lines >= options->minLines) {
-
-			buf[len++] = '\0';	
-
-			printf("%s ", filename);
-			printf("%d ", lines);
-			printf("%s\n", buf);
+			print_to_stdout(filename, lines, buf);
+			print_to_html(filename, lines, buf);
 		}
 	}
 	free(buf);
