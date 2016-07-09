82ede33be6f3c4f7a3e3cb508ea778de93855c2b
Horst Schneider <dieterdervermieter@gmail.com>
2016-07-09 13:17:34 +0200

    small changes

--- src/survey.h
+++ src/survey.h
@@ -1,4 +1,13 @@
+/*
+ * Opens single source file and prints survey to command line
+ * containing the filename, total number of lines and the 
+ * signature survey for that file.
+*/
 extern void print_sig_survey(const char*); 
+
+/*
+ * Callback for directory walker to pass files for survey printing.
+*/
 extern void do_survey(const char*);
 
 extern int total_statements;
